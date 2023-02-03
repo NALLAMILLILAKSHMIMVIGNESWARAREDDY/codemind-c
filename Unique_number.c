fba2313248a3b54e0718c11cd343594f9e512f7a
NALLAMILLILAKSHMIMVIGNESWARAREDDY <98519503+NALLAMILLILAKSHMIMVIGNESWARAREDDY@users.noreply.github.com>
2023-02-03 18:36:44 +0530

    #include <stdio.h> #include <stdbool.h>
    bool is_unique(int num) {
      int digits[10] = {0};
      while (num > 0) {
        int digit = num % 10;
        if (digits[digit] == 1) {
          return false;
        }
        digits[digit] = 1;
        num /= 10;
      }
      return true;
    }
    
    int main(void) {
      int num;
      scanf("%d", &num);
      if (is_unique(num)) {
        printf("Unique Number
    ");
      } else {
        printf("Not Unique Number
    ");
      }
      return 0;
    }

--- Unique_number.c
+++ Unique_number.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int main(){
-    int n, i, digit;
-    static int arr[10];
-    scanf("%d", &n);
-    while(n>0){
-        digit = n%10;
-        arr[digit]++;
-        n=n/10;
+bool is_unique(int num) {
+  int digits[10] = {0};
+  while (num > 0) {
+    int digit = num % 10;
+    if (digits[digit] == 1) {
+      return false;
     }
-    for(i=0; i<10; i++){
-        if(arr[i]>1){
-            printf("Not Unique Number");
-            break;
-        }
-    }
-    if(i==10){
-        printf("Unique Number");
-    }
-    return 0;
-  
- }
+    digits[digit] = 1;
+    num /= 10;
+  }
+  return true;
+}
+
+int main(void) {
+  int num;
+  scanf("%d", &num);
+  if (is_unique(num)) {
+    printf("Unique Number
+");
+  } else {
+    printf("Not Unique Number
+");
+  }
+  return 0;
+}
